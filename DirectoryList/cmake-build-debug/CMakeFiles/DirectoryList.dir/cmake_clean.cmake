e98a8e897c45e2e4d2d25b1b05521d1180479bb3
Hitoshi Hoshino <hhoshino-main@memoad.jp>
2018-07-31 18:44:48 +0900

    ls program first sample code commit.

---
+++ DirectoryList/cmake-build-debug/CMakeFiles/DirectoryList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DirectoryList.dir/main.cpp.o"
+  "DirectoryList.pdb"
+  "DirectoryList"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DirectoryList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
